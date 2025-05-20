96d5643075d4fdca54095fdd542da91ef41cb7bc
chYedukondalusrinivas <chyedukondalusrinivas@gmail.com>
2025-05-20 19:57:16 +0530

    Added branch b

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ class Hello{
     public static void main(String[] args) {
         System.out.println("Hello world from main branch");
         System.out.println("This is from branch a");
+        System.out.println("This is from branch b");
     }
 }
